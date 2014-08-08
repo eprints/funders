1d202b64d0260e54f916d7fd31c5db3fd113878e
Sebastien Francois <sf2@ecs.soton.ac.uk>
2014-08-08 13:52:49 +0100

    utility script to empty projects and funders datasets - useful when dev/debugging

---
+++ bin/empty_funders_projects_datasets.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w -I/opt/eprints/eprints3/perl_lib
+
+use EPrints;
+use strict;
+
+my $repo = EPrints->new->repository( $ARGV[0] );
+die "Usage: $0 [repoid]\n" if !defined $repo;
+
+foreach( 'funder', 'project' )
+{
+print "Comment-out code to use that script (danger zone)\n";
+#
+#        $repo->dataset( $_ )->search()->map( sub { $_[2]->remove } );
+}
+
