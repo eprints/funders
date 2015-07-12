5d4768f6a6d2042e9ba320512ea65dc255113457
David R Newman <drn@ecs.soton.ac.uk>
2015-07-13 00:16:59 +0100

    Amended historical projects/funders migration script to use different field names.

--- bin/copy_funders_projects_historical.pl
+++ bin/copy_funders_projects_historical.pl
@@ -15,47 +15,49 @@ my $projects_sql = "SELECT * FROM eprint_projects ORDER BY eprintid";
 my $projects_query = $db->prepare_select( $projects_sql );
 $db->execute( $projects_query, $projects_sql );
 my $eprint_project = $projects_query->fetchrow_hashref;
+print "Migrating old projects values to historical_projects";
 while( defined $eprint_project->{eprintid} )
 {
 	my $eprint = $ds->dataobj( $eprint_project->{eprintid} );
-	my @eprint_projects_historical = ();
+	my @eprint_historical_projects = ();
 	$last_eprintid = $eprint_project->{eprintid};
 	while ( defined $eprint_project->{eprintid} && $eprint_project->{eprintid} == $last_eprintid ) 
 	{
-		push @eprint_projects_historical, $eprint_project->{projects};
+		push @eprint_historical_projects, $eprint_project->{projects};
 		$eprint_project = $projects_query->fetchrow_hashref;
 	}
 	$last_eprintid = $eprint_project->{eprintid};
-	if ( defined $eprint && scalar @eprint_projects_historical > 0 )
+	if ( defined $eprint && scalar @eprint_historical_projects > 0 )
 	{
-		$eprint->set_value( "projects_historical",  \@eprint_projects_historical );
-		print STDERR "PROJECTS Commiting EPrint $last_eprintid\n";
+		$eprint->set_value( "historical_projects",  \@eprint_historical_projects );
 		$eprint->commit;
+		print ".";
 	}
 }
+print "Done!\n";
 	 
 my $funders_sql = "SELECT * FROM eprint_funders ORDER BY eprintid";
 my $funders_query = $db->prepare_select( $funders_sql );
 $db->execute( $funders_query, $funders_sql );
 my $eprint_funder = $funders_query->fetchrow_hashref;
+print "Migrating old funders values to historical_funders";
 while( defined $eprint_funder->{eprintid} )
 {
         my $eprint = $ds->dataobj( $eprint_funder->{eprintid} );
-        my @eprint_funders_historical = ();
+        my @eprint_historical_funders = ();
         $last_eprintid = $eprint_funder->{eprintid};
         while ( defined $eprint_funder->{eprintid} && $eprint_funder->{eprintid} == $last_eprintid )
         {
-                push @eprint_funders_historical, $eprint_funder->{funders};
+                push @eprint_historical_funders, $eprint_funder->{funders};
                 $eprint_funder = $funders_query->fetchrow_hashref;
         }
 	$last_eprintid = $eprint_funder->{eprintid};
-        if ( defined $eprint && scalar @eprint_funders_historical > 0 )
+        if ( defined $eprint && scalar @eprint_historical_funders > 0 )
         {
-		$eprint->set_value( "funders_historical",  \@eprint_funders_historical );
-		print STDERR "FUNDERS Commiting EPrint $last_eprintid\n";
+		$eprint->set_value( "historical_funders",  \@eprint_historical_funders );
               	$eprint->commit;
+		print ".";
         }
 }
+print "Done!\n";
 
-
-# To be written
