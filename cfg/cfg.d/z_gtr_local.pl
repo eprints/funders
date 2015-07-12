d1476b7514ff16623a43871a98b6bff9c9e834ed
David R Newman <drn@ecs.soton.ac.uk>
2015-07-13 00:15:59 +0100

    Removed erroneous added archive config for GtR stuff.

--- cfg/cfg.d/z_gtr_local.pl
+++
@@ -1,7 +0,0 @@
-
-# GtR unique identifiers for your institution - you may have more than one 
-# as per the example below for the Uni of Southampton
-$c->{gtr}{'data-import'} => [
-#		'30A429E3-83B7-4E41-99C0-14A144F07DFE',		# University of Southampton
-#		'4DB630C7-7E13-4610-A1C3-29601903CEE3',		# National Oceanography Centre
-];
